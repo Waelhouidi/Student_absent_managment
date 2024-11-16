55468beb84cf48d2122ee5394410ce9c01d2d73c
Waelhouidi <waelhwidi0@gmail.com>
2024-11-16 22:27:20 +0100

    create classes

---
+++ C_Etudiant.php
@@ -0,0 +1,12 @@
+<?php
+class Etudiant{
+    private $nom;
+    private $prenom;
+    private $adresse;
+    private $telephone;
+    private $email;
+    private $dateNaissance;
+    private $codeClass;
+    private $numInscription;
+}
+?>
